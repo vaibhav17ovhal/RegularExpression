37cb7dba571443497058af6af952b1182f177c08
Vaibhav <vaibhavovhal15@gmail.com>
2023-03-28 13:40:27 +0530

    [Vaibhav] Add:Pin code

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RegularExpression
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Regular Expression");
+            Console.WriteLine();
+
+            //UC1
+            ValidatePinCode pin = new ValidatePinCode();
+            pin.Validate("400088");       
+        }
+    }
+}
---
+++ RegularExpression/ValidatePinCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpression
+{
+    public class ValidatePinCode
+    {
+        public const string PIN_CODE = "^[0-9]{6}$";
+        
+        public void Validate(string pinCode)
+        {
+            if (Regex.IsMatch(pinCode, PIN_CODE))
+            {
+                Console.WriteLine("Validation Successfull");
+            }
+            else
+            {
+                Console.WriteLine("Validation UnSuccessfull");
+            }
+        }
+    }
+}
